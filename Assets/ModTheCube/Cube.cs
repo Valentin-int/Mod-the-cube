bed598bd75d988952ca473fa61696e4c84d2e67a
Valentin LAY <valentin.lay98@gmail.com>
2022-06-07 20:36:22 +0200

    Finish the challenge

--- Assets/ModTheCube/Cube.cs
+++ Assets/ModTheCube/Cube.cs
@@ -5,19 +5,31 @@ using UnityEngine;
 public class Cube : MonoBehaviour
 {
     public MeshRenderer Renderer;
-    
+    public Vector3 objectPosition = new Vector3(2, 6, 2);
+    public Vector3 objectSize = Vector3.one * 1.3f;
+
     void Start()
     {
-        transform.position = new Vector3(3, 4, 1);
-        transform.localScale = Vector3.one * 1.3f;
+        transform.position = objectPosition;
+        transform.localScale = objectSize;
         
         Material material = Renderer.material;
         
-        material.color = new Color(0.5f, 1.0f, 0.3f, 0.4f);
+        material.color = new Color(
+        Random.Range(0.2f, 0.9f),
+        Random.Range(0.1f, 0.4f),
+        Random.Range(0.5f, 0.9f),
+        Random.Range(0.5f, 0.6f)
+        );
     }
     
     void Update()
     {
-        transform.Rotate(10.0f * Time.deltaTime, 0.0f, 0.0f);
+        transform.Rotate(
+            Random.Range(5f, 15f)
+            * Time.deltaTime, 
+            Random.Range(1f, 5f), 
+            0
+            );
     }
 }
